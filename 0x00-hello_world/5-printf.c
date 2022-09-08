571088152a5d984cf7034dbf09d3c3209ba90e6f
mujieedah <Mujidat355@gmail.com>
2022-09-08 12:27:39 -0700

    use printf to print a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point, a c programming lang that prints
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
+return (0);
