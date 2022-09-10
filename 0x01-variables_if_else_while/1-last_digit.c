bfb47a7ba70f0ce418dd4959081081344fe8dc71
mujieedah <Mujidat355@gmail.com>
2022-09-10 15:45:59 -0700

    print random lSt numbef

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,23 @@
 int main(void)
 {
 	int n;
-	int lastdigit;
+	int lastdgit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
+	lastdgit = n % 10;
 
-	if (lastdigit > 5)
+	if (lastdgit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastdgit);
 	}
-	else if (lastdigit == 0)
+	else if (lastdgit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastdgit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdgit);
 	}
 	return (0);
 }
