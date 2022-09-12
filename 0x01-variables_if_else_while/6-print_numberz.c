5925e104eaf01b369b875c9c62c374bac5976ace
mujieedah <Mujidat355@gmail.com>
2022-09-12 03:10:10 -0700

    print singles of 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return o
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	putchar('0' + i);
+	putchar('\n');
+	return (0);
+}
