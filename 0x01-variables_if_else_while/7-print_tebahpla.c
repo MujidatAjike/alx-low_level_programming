1b637eed38c4ee162d19afd01ea15dea50d7952b
mujieedah <Mujidat355@gmail.com>
2022-09-12 03:20:25 -0700

    print a to z in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alaays return zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	putchar(ch);
+	pitchar('\n');
+	return (0);
+}
