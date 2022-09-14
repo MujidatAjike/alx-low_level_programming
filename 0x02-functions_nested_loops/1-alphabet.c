86a43aa0135a128f6a7b574dce1eaf214de2a5b7
mujieedah <Mujidat355@gmail.com>
2022-09-14 03:51:30 -0700

    my first prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always return zero
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
