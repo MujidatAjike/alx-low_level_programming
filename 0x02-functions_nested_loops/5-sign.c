7f5b536ad0285155daa20619fe3ae18b2fc3e227
mujieedah <Mujidat355@gmail.com>
2022-09-15 11:22:10 -0700

    compare n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ * @n: value compared
+ *
+ * Return: return 1 if true, 0 if false
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
