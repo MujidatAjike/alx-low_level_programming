78aaee8cdd75daa381fa9037fc1634ebac68cf5f
mujieedah <Mujidat355@gmail.com>
2022-09-15 08:15:41 -0700

    prototype

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,17 @@
  * description: print lower case alphabetg 10 times
  * Return: always return zero
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int q;
 	int s;
 
 	for (s = 0; s <= 9; s++)
 	{
-	for (q = 'a'; q = 'z'; q++)
-	{
-		 _putchar(q);
+		for (q = 'a'; q <= 'z'; q++)
+		{
+			_putchar(q);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	}
-	return (0);
 }
