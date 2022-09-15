93e84aac8d184b66369c202891c34e8801115af3
mujieedah <Mujidat355@gmail.com>
2022-09-14 23:55:54 -0700

    compare character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - main entry
+ *
+ * @c: character to compare
+ *
+ * Retirn: return 1 if true, 0 if false
+ */
+int _islower(int c);
+{
+	if (c >= 'a'; && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
