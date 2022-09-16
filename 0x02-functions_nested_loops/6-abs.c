c4bec525d98119c0c2fe66adf3732774d17f4092
mujieedah <Mujidat355@gmail.com>
2022-09-16 07:52:58 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - main function
+ * @n: value to be compared
+ * Return: return zero always
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
