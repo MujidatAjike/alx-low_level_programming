03ae58717c50519e91fe3db67d25c7a50b6629a9
mujieedah <Mujidat355@gmail.com>
2022-09-16 08:12:31 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - main function
+ * @r: intrger ti retirn last digit
+ * Return: always return last digit number
+ */
+int print_last_digit(int r)
+{
+	_putchar('0' + _abs(r % 10));
+	return (_abs(r % 10));
+}
