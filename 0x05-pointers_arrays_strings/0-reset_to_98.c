6b035b595a7e6d5a73281e854352725fa3d850d2
mujieedah <Mujidat355@gmail.com>
2022-09-20 10:02:30 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - check code
+ * @n: pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - check code
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int q = *a;
+	int f = *b;
+	*a = f;
+	*b = q;
+}
