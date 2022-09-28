f97918ec629a72382f5cde39bb7f76afa6e5e2b6
mujieedah <Mujidat355@gmail.com>
2022-09-28 09:29:38 -0700

    pointers, arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,17 +4,17 @@
  *
  * @dest: destination of concat
  * @src: sorce array to concat
+ * @n: amount of times to append
  *
- * Return: desr
- * */
+ * Return: dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
-	for(j=0; dest[j] != '\0'; j++)
+	for (j = 0; dest[j] != '\0'; j++)
 	{}
-	
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[j + i] = src[i];
